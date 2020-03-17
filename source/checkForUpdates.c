57203b0aadc0bf06ea89b86ca1df8fd46af9df8e
16BitWonder <ericthethree3@gmail.com>
2020-03-17 18:40:25 -0400

    Print time generated in Available-Updates.txt

--- source/checkForUpdates.c
+++ source/checkForUpdates.c
@@ -5,6 +5,14 @@ void checkForUpdates(FILE *out, Entry *localEntries, Entry *extEntries)
 	Entry *tmpLocal = localEntries->next;
 	Entry *tmpExt;
 	
+	u64 rawTime;
+	TimeCalendarTime timeCal;
+	TimeCalendarAdditionalInfo timeInfo;
+	timeGetCurrentTime(TimeType_UserSystemClock, &rawTime);
+	timeToCalendarTimeWithMyRule(rawTime, &timeCal, &timeInfo);
+	
+	fprintf(out, "Generated: %04i/%02i/%02i - %02i:%02i:%02i (%s)\n", timeCal.year, timeCal.month, timeCal.day, timeCal.hour, timeCal.minute, timeCal.second, timeInfo.timezoneName);
+	
 	fprintf(out, "Updates available:\n\n");
 	
 	while (tmpLocal != NULL)
