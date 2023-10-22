74a1d56426b36f602d0b9d58ef727e0ac06895f8
8BitWonder <ericthethree3@gmail.com>
2023-10-22 15:03:32 -0400

    Update main.cpp with more startup outlines

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,20 @@ int main(int argc, char **argv) {
 	controller.Init();
 	
 	// Begin appletMainLoop
+	VersionDB localVersions;
+	VersionDB externalVersions;
+	bool runOnStartup = true;
 	while(appletMainLoop()) {
+		
+		// Init Local Version List
+		if (runOnStartup) {
+			// TODO
+			// Update cartridge version List file with inserted cartridge
+			// Init localVersions
+			// Declare/Init cartVersions
+			// Update localVersions with cartVersions
+			// Init externalVersions (should account for missing versions.txt)
+		}
 		
 		// Update input
 		u64 kPressed = controller.getNewPressed();
